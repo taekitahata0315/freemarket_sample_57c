b1ef16d4a7531cc93255d88e14a750ae7e719bde
kunsay <kunsay0326@hotmail.co.jp>
2019-09-03 12:44:37 +0900

    ユーザーのマイページの初期画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'mercari#index'  
-  get '/mercari/logout', to: "mercari#logout", as: "mercari"
   resources :mercari
   resources :sell
   resources :users ,only: [:show]
+  resources :mypage
 end
 
 
