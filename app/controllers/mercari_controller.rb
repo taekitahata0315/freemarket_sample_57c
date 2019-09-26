63da1091dc2ce0b801d48ae92a03c1de8d97d5e8
mabuchi1 <msanhm6@gmail.com>
2019-09-26 17:56:31 +0900

    fix

--- app/controllers/mercari_controller.rb
+++ app/controllers/mercari_controller.rb
@@ -1,12 +1,12 @@
 class MercariController < ApplicationController
 
   def index
-    @items = Item.all
+    @items = Item.where(buyer_id: nil)
   end
 
   def show
     @item = Item.find(params[:id])
-    @items = Item.all
+    @items = Item.where(buyer_id: nil)
   end
 
   def logout
